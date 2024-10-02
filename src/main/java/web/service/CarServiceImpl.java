5788c49e367d5857bdc9bb0839d2e373423f0ff4
snowphoenix347 <snowphoenix347@gmail.com>
2024-10-02 18:44:41 +0300

    Практическая задача 2.2.2 MVC-приложение.

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,24 @@
+package web.service;
+
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarServiceImpl implements CarService {
+
+    private final List<Car> cars = new ArrayList<>();
+
+    public CarServiceImpl() {
+        cars.add(new Car(1992, "Toyota", "White gold"));
+        cars.add(new Car(2001, "BMW", "Blue sky"));
+        cars.add(new Car(1997, "Porsche", "Roared grey"));
+        cars.add(new Car(2015, "Mitsubishi", "Midori"));
+        cars.add(new Car(1976, "Chevrolet", "Black space"));
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+        return cars.subList(0, Math.min(count, cars.size()));
+    }
+}
